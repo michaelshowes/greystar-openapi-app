6d611813d72194406c07bd9e97c1b2e1a708524f
Michael Showes <michael.showes@gmail.com>
2025-10-17 13:21:32 -0400

    made map page dynamic

--- src/app/map/page.tsx
+++ src/app/map/page.tsx
@@ -1,5 +1,7 @@
 import Map from '@/components/Map';
 
+export const dynamic = 'force-dynamic';
+
 export default async function MapPage() {
 	const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/properties`);
 	const properties = await res.json();
